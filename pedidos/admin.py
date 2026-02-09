53d60afa6cd57219c3d1c0f83c9830885d758f00
vinicius wilker <vinicius.zoom360@gmail.com>
2026-02-09 19:29:23 -0300

    add: config inicial de rotas

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -3,8 +3,5 @@ from .models import Pedido, ItemPedido
 # Register your models here.
 
 
-@admin.register(Pedido)
-class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'data', 'status', 'total')
-    list_filter = ('status',)
-    inlines = [ItemPedidoInline]
+admin.site.register(Pedido)
+admin.site.register(ItemPedido)
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def login(request):
+    pass
+
+
+def home(request):
+    pass
+
+
+def criarPedido(request):
+    pass
+
