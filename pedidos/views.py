e0734c5fbfddc516ff6b10ef1a32803543825487
vinicius wilker <vinicius.zoom360@gmail.com>
2026-02-09 19:41:42 -0300

    feat: funcao de login

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
+def login_view(request):
 
-def login(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
 
+        if not usuario or not senha:
+            messages.error(request, "Preencha todos os dados.")
+            return (request, 'pedidos/login.html')
+
+        
+        user = authenticate(request, username=usuario, password=senha)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Sua conta está desativada.")
+        else:
+            messages.error(request, "Credenciais inválidas. Tente novamente.")
+
+    return render(request, 'pedidos/login.html')
+
+    
 
 def home(request):
     pass
 
 
-def criarPedido(request):
-    pass
-
+def logout_view(request):
+    logout(request)
+    return redirect('login')
