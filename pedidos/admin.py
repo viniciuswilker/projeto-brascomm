65562e3ea2f6fc53313435203c9aa390df343aaf
vinicius wilker <vinicius.zoom360@gmail.com>
2026-02-09 19:20:10 -0300

    model registrada no admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Pedido, ItemPedido
 # Register your models here.
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data', 'status', 'total')
+    list_filter = ('status',)
+    inlines = [ItemPedidoInline]
